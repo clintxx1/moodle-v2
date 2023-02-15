b88f871cf4edd7914edb59865b1c866e3c8d9311
Clint <clint.m@freshclinics.com.au>
2023-02-15 23:58:58 +0800

    fix: text positions on course container

--- src/components/course.jsx
+++ src/components/course.jsx
@@ -5,7 +5,7 @@ import { EllipsisOutlined } from "@ant-design/icons";
 const Course = ({ dept, subject, progress }) => {
   return (
     <div className="flex flex-row items-center w-full border-gray-300 border-x-[1px] border-y-[1px] my-3 min-h-[80px]">
-      <div className="hidden md:flex md:flex-row w-full">
+      <div className="hidden md:flex md:flex-row w-full ">
         <Col span={22}>
           <Row gutter={12}>
             <Col span={12}>
@@ -33,19 +33,19 @@ const Course = ({ dept, subject, progress }) => {
       </div>
       <div className="flex flex-row items-center w-full md:hidden">
         <Col span={22}>
-          <Row gutter={12}>
+          <Row>
             <Col span={12}>
-              <div className="opacity-75 pl-5 pt-3">{dept}</div>
+              <div className="opacity-75 pl-5">{dept}</div>
             </Col>
             <Col span={12}>
-              <div className="pl-5 pb-3 hover:underline inline-block cursor-pointer">
+              <div className="pl-5 hover:underline inline-block cursor-pointer">
                 {subject}
               </div>
             </Col>
           </Row>
           <Row>
             <Col span={24}>
-              <div className="mt-2 mr-5">
+              <div className="mt-2 mx-5">
                 <Progress percent={progress} />
               </div>
             </Col>
